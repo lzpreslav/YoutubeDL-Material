e63774a285cb79af3a6361604ebac7b6d0844811
lzpreslav <lzpreslav@gmail.com>
2026-01-18 21:56:42 +0200

    chore: clean up dockerfile
    - use mounts when files are not needed in a layer
    - combine installing packages to save on space
    - improve readability of longer commands
    - use cache mounts for npm dependencies
    - update nvm
    - install yt-dlp-ejs (forgotten dependency)
    - chown only the appdata folder on startup, let users manage their own library permissions

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -eu
 
-# chown current working directory to current user
-echo "[entrypoint] setup permission, this may take a while"
-find . \! -user "$UID" -exec chown "$UID:$GID" '{}' + || echo "WARNING! Could not change directory ownership. If you manage permissions externally this is fine, otherwise you may experience issues when downloading or deleting videos."
+echo "[entrypoint] setup appdata permissions"
+chown -R "$UID:$GID" /app/appdata || echo "WARNING! Could not change appdata ownership"
 exec gosu "$UID:$GID" "$@"
